ab4c4ff84f043be22b731b8bb3661ee7a1428a35
Kartik-007 <masterkartik1@gmail.com>
2026-02-26 22:28:19 -0500

    Add SHA256 to release workflow and Cask file

--- homebrew/flowprompt.rb
+++ homebrew/flowprompt.rb
@@ -1,6 +1,6 @@
 cask "flowprompt" do
   version "1.0.0"
-  sha256 "REPLACE_WITH_SHA256_OF_RELEASE_ZIP"
+  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
 
   url "https://github.com/kartikmehra/FlowPrompt/releases/download/v#{version}/FlowPrompt.zip"
   name "FlowPrompt"
